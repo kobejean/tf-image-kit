b2275bc7a7cf6df13474993973f59e33fe91d26f
kobejean <kobejean@me.com>
2017-06-17 22:53:27 -0600

    Weights & housekeeping

--- kmeans.py
+++ kmeans.py
@@ -8,9 +8,10 @@ from PIL import Image
 # adapted from https://gist.github.com/dave-andersen/265e68a5e879b5540ebc
 # and https://github.com/meereeum/k-meanz/
 
-MAX_ITERS = 1000
+MAX_ITERS = 10
+PERIOD = 1
 
-def kmeans(filepath, k, outdir = None):
+def kmeans(filepath, k, wx = 1.0, wy = 1.0, outdir = None):
     start = time.time()
 
     is_jpeg = filepath.lower().endswith("jpg") or filepath.lower().endswith("jpeg")
@@ -27,23 +28,26 @@ def kmeans(filepath, k, outdir = None):
 
 
     with tf.Session() as session:
-        # session.run(init)
-
 
         with open(filepath, 'rb') as f:
-            img_str = f.read()
-        pixels = decoder(img_str)
+            input_image = f.read()
+
+        pixels = decoder(input_image)
         m, n, chann = tf.shape(pixels).eval()
         idxs = tf.constant([(j,k) for j in range(m) for k in range(n)], dtype=tf.float32)
-        pix_array = tf.concat([idxs, tf.to_float(tf.reshape(pixels, shape=(m * n, chann)))], 1)
+        pixel_array = tf.concat([idxs, tf.to_float(tf.reshape(pixels, shape=(m * n, chann)))], 1)
+        # weight the values
+        weights = (wx/float(m), wy/float(n), 1.0/255.0, 1.0/255.0, 1.0/255.0)
+        weight_mold = tf.cast(tf.tile([weights], [m * n, 1]), tf.float32)
+        weighted_pix_array = tf.multiply(weight_mold, pixel_array)
 
-        cluster_assignments = tf.Variable(tf.zeros([m * n], dtype=tf.int64))
-        centroids = tf.Variable(tf.slice(tf.random_shuffle(pix_array), [0,0], [k,-1]), name="centroids")
+        cluster_assignments = tf.Variable(tf.zeros([m * n], dtype=tf.int64), name="cluster_assignments")
+        centroids = tf.Variable(tf.slice(tf.random_shuffle(weighted_pix_array), [0,0], [k,-1]), name="centroids")
 
         # Replicate to N copies of each centroid and K copies of each
         # point, then subtract and compute the sum of squared distances.
         rep_centroids = tf.reshape(tf.tile(centroids, [m * n, 1]), [m * n, k, 5])
-        rep_pix = tf.reshape(tf.tile(pix_array, [1, k]), [m * n, k, 5])
+        rep_pix = tf.reshape(tf.tile(weighted_pix_array, [1, k]), [m * n, k, 5])
         sum_squares = tf.reduce_sum(tf.square(rep_pix - rep_centroids), axis=2)
 
         # Use argmin to select the lowest-distance point
@@ -55,7 +59,7 @@ def kmeans(filepath, k, outdir = None):
             count = tf.unsorted_segment_sum(tf.ones_like(data), bucket_ids, num_buckets)
             return total / count
 
-        means = bucket_mean(pix_array, best_centroids, k)
+        means = bucket_mean(weighted_pix_array, best_centroids, k)
 
         # Do not write to the assigned clusters variable until after
         # computing whether the assignments have changed - hence with_dependencies
@@ -72,87 +76,58 @@ def kmeans(filepath, k, outdir = None):
 
         while changed and iters < MAX_ITERS:
             iters += 1
+            print()
             print("ITERATION:", iters)
             [changed, _] = session.run([did_assignments_change, do_updates])
 
-
-            if iters % 10 == 0:
-                now = datetime.datetime.today().strftime('%Y%m%d_%H%M')
-                outpath = os.path.join(outdir, '{}_{}_k{}_{}{}'.format(basename, now, k, iters, ext))
+            if iters % PERIOD == 0:
+                now = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+                outpath = os.path.join(outdir, '{}_{}_k{}_wx{}_wy{}_i{}{}'.format(basename, now, k, wx, wy, iters, ext))
                 [centers, assignments] = session.run([centroids, cluster_assignments])
                 end = time.time()
-                print(("Found in %.2f seconds" % (end-start)), iters, "iterations")
-                print("Centroids:")
+                print("CENTROIDS:")
                 print(centers)
-                print("Cluster assignments:", assignments)
-
-                print("Period rgbs:")
-                i_centroids = tf.cast(tf.round(centers), tf.uint8)
+                print("ASSIGNMENTS:", assignments)
+                # reverse weights
+                unweight_mold = 1 / tf.cast(tf.tile([weights], [k, 1]), tf.float32)
+                unweighted_centers = tf.multiply(centers, unweight_mold)
+                # assign rgb values from centroids
+                i_centroids = tf.cast(tf.round(unweighted_centers), tf.uint8)
                 cluster_gather = tf.gather(i_centroids, assignments)
+                # (m,n,R,G,B) becomes just (R,G,B)
                 rgb_slice = tf.slice(cluster_gather, [0,2], [-1,-1])
-                print(session.run(cluster_gather))
+                print("RGB:")
                 print(session.run(rgb_slice))
+                final_pixel_array = tf.reshape(rgb_slice, [m, n, chann])
 
-                fin_pix = tf.reshape(rgb_slice, [m, n, chann])
-                # # print(session.run(tf.shape(pix_array)))
-                image = session.run(encoder(fin_pix))
-
+                # encode
+                image = session.run(encoder(final_pixel_array))
                 with open(outpath, 'wb') as f:
                     f.write(image)
 
-        now = datetime.datetime.today().strftime('%Y%m%d_%H%M')
-        outpath = os.path.join(outdir, '{}_{}_k{}_{}{}'.format(basename, now, k, iters, ext))
 
-        [centers, assignments] = session.run([centroids, cluster_assignments])
-        end = time.time()
-        print(("Found in %.2f seconds" % (end-start)), iters, "iterations")
-        print("Centroids:")
-        print(centers)
-        print("Cluster assignments:", assignments)
-
-        print("Final rgbs:")
-        i_centroids = tf.cast(tf.round(centers), tf.uint8)
-        cluster_gather = tf.gather(i_centroids, assignments)
-        rgb_slice = tf.slice(cluster_gather, [0, 2], [-1, -1])
-        print(session.run(i_centroids))
-        print(session.run(rgb_slice))
-
-        fin_pix = tf.reshape(rgb_slice, [m, n, chann])
-        # # print(session.run(tf.shape(pix_array)))
-        image = session.run(encoder(fin_pix))
-
-        with open(outpath, 'wb') as f:
-            f.write(image)
-
-
-
-def _image_to_data(filepath, decoder):
-        """Convert image to 1D array of image data: (m, n, R, G, B) per pixel"""
-        with open(filepath, 'rb') as f:
-            img_str = f.read()
-        pixels = decoder(img_str)
-        m, n, chann = tf.shape(pixels).eval()
-        idxs = tf.constant([(j,k) for j in range(m) for k in range(n)], dtype=tf.float32)
-        return tf.concat([idxs, tf.to_float(tf.reshape(pixels, shape=(m * n, chann)))], 1)
-
-# filepath="output/input/img.png"
-# print(os.path.expanduser(filepath))
-# print(os.path.splitext(os.path.basename(filepath))[0])
 print("RUNNING")
 abspath = os.path.abspath(os.path.dirname(__file__))
 inputpath = os.path.join(abspath, "input")
 outpath = os.path.join(abspath, "output")
+
 ntreadpath = os.path.join(inputpath, "nagano_train.jpeg")
 kbreadpath = os.path.join(inputpath, "kurohime_bike.jpeg")
 
-# kmeans(ntreadpath, 10, outpath)
-# kmeans(ntreadpath, 20, outpath)
-# kmeans(ntreadpath, 30, outpath)
-# kmeans(ntreadpath, 40, outpath)
-# kmeans(ntreadpath, 50, outpath)
+# kmeans(kbreadpath, 10, 0.0, 0.0, outpath)
 
-kmeans(kbreadpath, 10, outpath)
-kmeans(kbreadpath, 20, outpath)
-kmeans(kbreadpath, 30, outpath)
-kmeans(kbreadpath, 40, outpath)
-kmeans(kbreadpath, 50, outpath)
+# for i in range(10,100,10):
+#     kmeans(ntreadpath, i, 0.0, 0.0, outpath)
+#     kmeans(ntreadpath, i, 0.1, 0.1, outpath)
+#     kmeans(ntreadpath, i, 0.5, 0.5, outpath)
+#     kmeans(ntreadpath, i, 1.0, 1.0, outpath)
+#     kmeans(ntreadpath, i, 0.0, 1.0, outpath)
+#     kmeans(ntreadpath, i, 1.0, 0.0, outpath)
+
+for i in range(10,100,10):
+    kmeans(kbreadpath, i, 0.0, 0.0, outpath)
+    kmeans(kbreadpath, i, 0.1, 0.1, outpath)
+    kmeans(kbreadpath, i, 0.5, 0.5, outpath)
+    kmeans(kbreadpath, i, 1.0, 1.0, outpath)
+    kmeans(kbreadpath, i, 0.0, 1.0, outpath)
+    kmeans(kbreadpath, i, 1.0, 0.0, outpath)
